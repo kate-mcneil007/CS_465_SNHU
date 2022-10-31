302f636ba8df57f6cbe19e3fb0dd40efcbab4ffa
kate-mcneil007 <kate.mcneil007@gmail.com>
2022-10-31 15:58:34 -0400

    commit

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -3,7 +3,8 @@ var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
 
 /* GET travel view */
 const travel = (req, res) => {
-    res.render('travel', {title : 'Travlr Getaways', trips });
+    pageTitle = process.env.npm_package_description + ' - Travel';
+    res.render('travel', {title : pageTitle, trips });
 };
 
 module.exports = {
