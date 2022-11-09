6a401b71d990bb255ad79b09c24c66351a640a04
kate-mcneil007 <kate.mcneil007@gmail.com>
2022-11-09 10:38:24 -0500

    Mod3 Commit

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,10 +1,13 @@
-var fs = require('fs');
-var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+//var fs = require('fs');
+//var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+const fs = require('fs');
+const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
 
 /* GET travel view */
 const travel = (req, res) => {
-    pageTitle = process.env.npm_package_description + ' - Travel';
-    res.render('travel', {title : pageTitle, trips });
+    //pageTitle = process.env.npm_package_description + ' - Travel';
+    pageTitle = 'Kathryn McNeil - Travel';
+    res.render('travel', {title : pageTitle, trips});
 };
 
 module.exports = {
