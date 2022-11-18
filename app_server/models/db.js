299c0b18a885a75b69720783cb4575fe23220b52
kate-mcneil007 <kate.mcneil007@gmail.com>
2022-11-18 17:12:51 -0500

    changes

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,11 +1,21 @@
 const mongoose = require('mongoose');
-// Changed to travlr, created host
 const host = process.env.DB_HOST || '127.0.0.1'
+const dbURI = `mongodb://${host}/travlr`;
+const readLine = require('readline');
+/*
 let dbURI = 'mongodb://${host}/travlr';
 if (process.env.NODE_ENV === 'production') {
   dbURI = process.env.MONGODB_URI;
+  mongoose.connect(dbURI);
+}*/
+mongoose.set('useUnifiedTopology', true);
+
+const connect = () => {
+  setTimeout(() => mongoose.connect (dbURI, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+  }), 1000);
 }
-mongoose.connect(dbURI);
 
 mongoose.connection.on('connected', () => {
   console.log(`Mongoose connected to ${dbURI}`);
@@ -16,6 +26,17 @@ mongoose.connection.on('error', err => {
 mongoose.connection.on('disconnected', () => {
   console.log('Mongoose disconnected');
 });
+
+const readLine = require ('readline');
+if (process.platform === 'win32'){
+  const rl = readLine.createInterface ({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.on ('SIGINT', () => {
+    process.emit ("SIGINT");
+  });
+}
 
 const gracefulShutdown = (msg, callback) => {
   mongoose.connection.close( () => {
@@ -42,5 +63,7 @@ process.on('SIGTERM', () => {
     process.exit(0);
   });
 });
+
+connect();
 
 require('./travlr');
