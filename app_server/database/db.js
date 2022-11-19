d050390e0baa1e8a9716e518eae718aa9240a4d3
kate-mcneil007 <kate.mcneil007@gmail.com>
2022-11-19 12:32:08 -0500

    goose

--- app_server/database/db.js
+++ app_server/database/db.js
@@ -1,24 +1,17 @@
 const mongoose = require('mongoose');
 const host = process.env.DB_HOST || '127.0.0.1'
-const dbURI = `mongodb://${host}/travlr`;
+const dbURL = `mongodb://${host}/travlr`;
+//const dbURI = 'mongodb://localhost';
 const readLine = require('readline');
-/*
-let dbURI = 'mongodb://${host}/travlr';
-if (process.env.NODE_ENV === 'production') {
-  dbURI = process.env.MONGODB_URI;
-  mongoose.connect(dbURI);
-}*/
+
 mongoose.set('useUnifiedTopology', true);
 
 const connect = () => {
-  setTimeout(() => mongoose.connect (dbURI, {
-    useNewUrlParser: true,
-    useCreateIndex: true
-  }), 1000);
+  setTimeout(() => mongoose.connect(dbURL, { useNewUrlParser: true, useCreateIndex: true }), 1000);
 }
 
 mongoose.connection.on('connected', () => {
-  console.log(`Mongoose connected to ${dbURI}`);
+  console.log(`Mongoose connected to ${dbURL}`);
 });
 mongoose.connection.on('error', err => {
   console.log('Mongoose connection error:', err);
@@ -27,7 +20,7 @@ mongoose.connection.on('disconnected', () => {
   console.log('Mongoose disconnected');
 });
 
-const readLine = require ('readline');
+//const readLine = require ('readline');
 if (process.platform === 'win32'){
   const rl = readLine.createInterface ({
     input: process.stdin,
